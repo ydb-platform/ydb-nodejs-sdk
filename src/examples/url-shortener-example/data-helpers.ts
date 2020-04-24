8fb772f1518a209455aa5bf219fb5150bdae8716
Mikhail Sitnikov <mihanix.it@gmail.com>
2020-04-24 23:22:52 +0300

    url shortener example (#3)
    * new: url shortener example
    
    * fix some pr issues
    
    * fix dependencies

---
+++ src/examples/url-shortener-example/data-helpers.ts
@@ -0,0 +1,51 @@
+import {declareType, TypedData} from "../../types";
+import {Ydb} from "../../../proto/bundle";
+
+const SOURCE_URL_REGEX = new RegExp("https?://(?:[-\\w.]|(?:%[\\da-fA-F]{2}))+");
+const SHORTEN_REGEX = new RegExp("[a-zA-Z0-9]");
+
+let crc = require('crc');
+
+interface IUrlsMatch {
+    shorten: string,
+    source: string
+}
+export class UrlsMatch extends TypedData {
+    @declareType({typeId: Ydb.Type.PrimitiveTypeId.UTF8})
+    public shorten: string;
+
+    @declareType({typeId: Ydb.Type.PrimitiveTypeId.UTF8})
+    public source: string;
+
+    static isSourceUrlCorrect(sourceUrl: string): boolean {
+        return SOURCE_URL_REGEX.test(sourceUrl);
+    }
+
+    static calculateShortenUrl(sourceUrl: string): string {
+        return crc.crc32(sourceUrl, 0).toString(16);
+    }
+
+    constructor(data: IUrlsMatch) {
+        super(data);
+        this.shorten = data.shorten;
+        this.source = data.source;
+    }
+}
+
+
+interface IRequestSourceUrl {
+    shorten: string
+}
+export class RequestSourceUrl extends TypedData {
+    @declareType({typeId: Ydb.Type.PrimitiveTypeId.UTF8})
+    public shorten: string;
+
+    static isShortenCorrect(shorten: string): boolean {
+        return SHORTEN_REGEX.test(shorten);
+    }
+
+    constructor(data: IRequestSourceUrl) {
+        super(data);
+        this.shorten = data.shorten;
+    }
+}
