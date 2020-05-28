e25b109a3ee5cb7046bb544097d34a691571759a
Timur Sufiev <tsufiev@yandex-team.ru>
2020-05-28 16:28:16 +0300

    tune(index): re-export TokenAuthService from top-level

--- src/index.ts
+++ src/index.ts
@@ -20,5 +20,6 @@ export {
     CachingPolicy
 } from './table';
 export {getCredentialsFromEnv} from "./parse-env-vars";
+export {TokenAuthService} from './credentials';
 export {withRetries, RetryParameters} from "./retries";
 export {YdbError, StatusCode} from './errors';
