a209282adc0362450acb7249220fd7135fb0899e
Timur Sufiev <tsufiev@yandex-team.ru>
2020-12-17 16:33:22 +0300

    fix(DiscoveryService): correctly tell driver that endpoint is removed
    Use the same event name in DiscoveryService and in driver.

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,6 @@
 export const ENDPOINT_DISCOVERY_PERIOD = 2 * 60 * 1000; // 2 minutes
 export const SESSION_KEEPALIVE_PERIOD = 60 * 1000; // 1 minute
+
+export enum Events {
+    ENDPOINT_REMOVED = 'endpoint:removed'
+}
