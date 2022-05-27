83e2fe65cc5035e65d2281c41e1c0699bd08e845
Timur Sufiev <tsufiev@yandex-team.ru>
2022-05-27 13:32:39 +0600

    revert: "fix: correctly access package.json"
    This reverts commit 3638572d8352e39dcb0ab3ed8ab97e72507126f1.

--- src/version.ts
+++ src/version.ts
@@ -1,5 +1,4 @@
-const pkgPath = process.env.NODE_ENV === 'test' ? '../package.json' : '../../package.json';
-const pkgInfo = require(pkgPath);
+const pkgInfo = require('../package.json');
 
 function getVersion() {
     return pkgInfo.version;
