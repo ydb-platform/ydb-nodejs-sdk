013efb04e0ac667672ad03f3df5643662cafcc4e
jsus <esseswann@gmail.com>
2022-09-02 15:17:55 +0300

    fix: extracting version from imported package.json

--- fixup.sh
+++ fixup.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
+package_version=`npm show . version`
+
 cat >build/cjs/package.json <<!EOF
 {
+    "version": "$package_version",
     "type": "commonjs"
 }
 !EOF
 
 cat >build/esm/package.json <<!EOF
 {
+    "version": "$package_version",
     "type": "module"
 }
 !EOF
