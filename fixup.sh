c3f6b88b5e64747d3c639bd434d4d5a68f57c67f
jsus <esseswann@gmail.com>
2022-09-13 13:41:17 +0300

    fix: conditional target type

--- fixup.sh
+++ fixup.sh
@@ -4,10 +4,11 @@ package_version=`npm show . version`
 
 for target in esm cjs; do
     cp -R certs build/$target/src/
+    [[ $target == "esm" ]] && type="module" || type="commonjs"
     cat >build/$target/package.json <<!EOF
 {
     "version": "$package_version",
-    "type": "module"
+    "type": "$type"
 }
 !EOF
 done
