8c5c18ac7367b2e34de634ff9231e40035ecf214
Ilya Shevchenko <id.shev@yandex.ru>
2022-10-19 16:29:52 +0400

    fix(build): return certs back to bundle
    Return cp of certs into bundle. Cp was deleted in
    1304d4c76962ad8b26171a00f577efe560ca431c, but tsc
    did not include certs in the build.

--- fixup.sh
+++ fixup.sh
@@ -3,6 +3,7 @@
 package_version=`npm show . version`
 
 for target in esm cjs; do
+    cp -R certs build/$target/src
     cat >build/$target/package.json <<!EOF
 {
     "version": "$package_version"
