7791a930c6630b43a4320a549f1ed465574bba29
Ivan Zuev <dajohnes@gmail.com>
2022-10-19 15:04:10 +0300

    fix: inline certificates content

--- src/ssl-credentials.ts
+++ src/ssl-credentials.ts
@@ -1,25 +1,15 @@
+import fs from 'fs';
+import tls from 'tls';
+
 // noinspection ES6PreferShortImport
 import { Logger } from './logging';
-import fs from 'fs';
-import path from 'path';
-
-const CERTIFICATES_FOLDER = 'certs'
-const RELATIVE_PATH = process.env.TEST_ENVIRONMENT ? '../' : './'
-const RESOLVED_PATH = path.join(__dirname, RELATIVE_PATH, CERTIFICATES_FOLDER)
-const FALLBACK_INTERNAL_ROOT_CERTS = path.join(RESOLVED_PATH, 'internal.pem');
-const FALLBACK_SYSTEM_ROOT_CERTS = path.join(RESOLVED_PATH, 'system.pem');
+import * as certs from './certs';
 
 function makeInternalRootCertificates() {
-    if (!fs.existsSync(FALLBACK_INTERNAL_ROOT_CERTS)
-     || !fs.existsSync(FALLBACK_SYSTEM_ROOT_CERTS)) {
-        throw new Error(certificateNotFoundMessage)
-    }
-
-    const internalRootCertificates = fs.readFileSync(FALLBACK_INTERNAL_ROOT_CERTS)
-    const fallbackSystemRootCertificates = fs.readFileSync(FALLBACK_SYSTEM_ROOT_CERTS)
+    const internalRootCertificates = Buffer.from(certs.internal, 'utf8')
+    const fallbackSystemRootCertificates = Buffer.from(certs.system, 'utf8')
 
     let systemRootCertificates: Buffer;
-    const tls = require('tls');
     const nodeRootCertificates = tls.rootCertificates as string[] | undefined;
     if (nodeRootCertificates && nodeRootCertificates.length > 0) {
         systemRootCertificates = Buffer.from(nodeRootCertificates.join('\n'));
@@ -54,12 +44,6 @@ export function makeSslCredentials(endpoint: string, logger: Logger, sslCredenti
     }
     return makeDefaultSslCredentials();
 }
-
-const certificateNotFoundMessage = `No certificate found
-It seems that you are using grpcs (secure) endpoint in a bundled environment.
-Either provide YDB_SSL_ROOT_CERTIFICATES_FILE environment variable
-or copy contents of ydb-nodejs-sdk/certs to ./certs path relative to the bundled file
-`
 
 export interface ISslCredentials {
     rootCertificates?: Buffer,
