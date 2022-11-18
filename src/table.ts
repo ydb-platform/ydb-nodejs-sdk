5025b8455b1cf92a60881e2ae848e98b824d1439
Ilya Shevchenko <id.shev@yandex.ru>
2022-11-18 20:51:52 +0300

    feat: add data columns and sync/async index to TableIndex
    Add more properties to TableIndex class
    from proto file

--- src/table.ts
+++ src/table.ts
@@ -1095,14 +1095,34 @@ export class TableProfile implements Ydb.Table.ITableProfile {
 
 export class TableIndex implements Ydb.Table.ITableIndex {
     public indexColumns: string[] = [];
+    public dataColumns: string[] | null = null;
+    public globalIndex: Ydb.Table.IGlobalIndex|null = null;
+    public globalAsyncIndex: Ydb.Table.IGlobalAsyncIndex|null = null;
 
     constructor(public name: string) {}
 
     withIndexColumns(...indexColumns: string[]) {
-        for (const index of indexColumns) {
-            this.indexColumns.push(index);
-        }
+        this.indexColumns.push(...indexColumns);
         return this;
+    }
+
+    /** Adds [covering index](https://ydb.tech/en/docs/concepts/secondary_indexes#covering) over columns */
+    withDataColumns(...dataColumns: string[]) {
+        if(!this.dataColumns) this.dataColumns = []
+        this.dataColumns?.push(...dataColumns)
+        return this
+    }
+
+    withGlobalAsync(isAsync: boolean) {
+        if(isAsync) {
+            this.globalAsyncIndex = new Ydb.Table.GlobalAsyncIndex()
+            this.globalIndex = null
+        }
+        else {
+            this.globalAsyncIndex = null
+            this.globalIndex = new Ydb.Table.GlobalIndex()
+        }
+        return this
     }
 }
 
