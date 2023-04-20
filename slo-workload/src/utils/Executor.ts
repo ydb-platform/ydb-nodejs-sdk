bc151718f9aa0a6b4b609a416c364f3bdd2aef4b
Ilya Shevchenko <id.shev@yandex.ru>
2023-04-20 18:14:21 +0400

    ci(slo): include more params in executor
    add tableName, runTimeSecs, readTimeout, writeTimeout params in constructor,
    add queries and executeQuerySettings

--- slo-workload/src/utils/Executor.ts
+++ slo-workload/src/utils/Executor.ts
@@ -1,7 +1,7 @@
 import { writeFile } from 'fs/promises'
 import http from 'http'
 import { Gauge, Summary, Registry, Pushgateway } from 'prom-client'
-import { Driver, Session } from 'ydb-sdk'
+import { Driver, ExecuteQuerySettings, OperationParams, Session } from 'ydb-sdk'
 import { packages } from '../../package-lock.json'
 
 const sdkVersion = packages['node_modules/ydb-sdk'].version
@@ -17,10 +17,53 @@ export default class Executor {
   private readonly latencies: Summary
   private readonly gateway: Pushgateway
   private collectingMetrics: Boolean = true
-  public readonly realRPS: Gauge
+  readonly realRPS: Gauge
+  readonly tableName: string
+  readonly stopTime: number
+  readonly readExecuteQuerySettings: ExecuteQuerySettings
+  readonly writeExecuteQuerySettings: ExecuteQuerySettings
+  readonly readQuery: string
+  readonly writeQuery: string
 
-  constructor(driver: Driver, pushGateway: string) {
+  constructor(
+    driver: Driver,
+    pushGateway: string,
+    tableName: string,
+    runTimeSecs: number,
+    readTimeout: number,
+    writeTimeout: number
+  ) {
     this.driver = driver
+    this.tableName = tableName
+    this.stopTime = new Date().valueOf() + runTimeSecs * 1000
+
+    this.readQuery = `DECLARE $id AS Uint64;
+SELECT id, payload_str, payload_double, payload_timestamp, payload_hash
+FROM \`${this.tableName}\` WHERE id = $id AND hash = Digest::NumericHash($id);`
+    this.writeQuery = `DECLARE $id AS Uint64;
+DECLARE $payload_str AS Utf8;
+DECLARE $payload_double AS Double;
+DECLARE $payload_timestamp AS Timestamp;
+UPSERT INTO \`${this.tableName}\` (
+  id, hash, payload_str, payload_double, payload_timestamp
+) VALUES (
+  $id, Digest::NumericHash($id), $payload_str, $payload_double, $payload_timestamp
+);`
+    this.readExecuteQuerySettings = new ExecuteQuerySettings()
+      .withKeepInCache(true)
+      .withOperationParams(
+        new OperationParams().withOperationTimeout({
+          nanos: readTimeout * 1000 * 1000,
+        })
+      )
+    this.writeExecuteQuerySettings = new ExecuteQuerySettings()
+      .withKeepInCache(true)
+      .withOperationParams(
+        new OperationParams().withOperationTimeout({
+          nanos: writeTimeout * 1000 * 1000,
+        })
+      )
+
     this.gateway = new Pushgateway(
       pushGateway,
       {
