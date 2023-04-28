005e5b4b6de86a2274fd2cbf66d5e481d7f2cee9
Ilya Shevchenko <id.shev@yandex.ru>
2023-04-28 22:20:36 +0400

    chore(tests): add tests for BackoffSettings

--- src/retries.ts
+++ src/retries.ts
@@ -3,14 +3,24 @@ import {getLogger, Logger} from './logging';
 import * as errors from './errors';
 import {sleep} from './utils';
 
-
-class BackoffSettings {
-    constructor(public backoffCeiling: number, public backoffSlotDuration: number, private uncertainRatio = 0.5) {}
+export class BackoffSettings {
+    /**
+     * Create backoff settings - uses randomized exponential timeouts with a base of 2
+     * Timeout formula: `2^min(retries, backoffCeiling) * backoffSlotDuration * (1 - random() * uncertainRatio)`
+     * @param backoffCeiling - max power — (n) in `2^n`
+     * @param backoffSlotDuration - multiplier for exponent
+     * @param uncertainRatio - timeout fraction that is randomized
+     */
+    constructor(
+        public backoffCeiling: number,
+        public backoffSlotDuration: number,
+        private uncertainRatio = 0.5,
+    ) {}
 
     async waitBackoffTimeout(retries: number) {
         const slotsCount = 1 << Math.min(retries, this.backoffCeiling);
         const maxDuration = slotsCount * this.backoffSlotDuration;
-        const duration = maxDuration * (Math.random() * this.uncertainRatio + 1 - this.uncertainRatio);
+        const duration = maxDuration * (1 - Math.random() * this.uncertainRatio);
         return sleep(duration);
     }
 }
