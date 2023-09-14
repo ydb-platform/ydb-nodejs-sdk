a440f4c7f403da6ab43a8b5a962ced217b627120
Alexey Zorkaltsev <AZorkaltsev@Yandex-Team.ru>
2023-09-14 18:59:41 +0300

    docs: add examples for the article

---
+++ examples/transaction-control/serializable.ts
@@ -0,0 +1,19 @@
+import {Driver, TokenAuthService} from 'ydb-sdk';
+
+(async function () {
+    const driver = new Driver({
+        connectionString: process.env.YDB_CONNECTION_STRING,
+        authService: new TokenAuthService(process.env.YDB_TOKEN as string),
+    });
+    try {
+        await driver.tableClient.withSession(async (session) => {
+            const preparedQuery = await session.prepareQuery("SELECT 1");
+            const txMeta = await session.beginTransaction({serializableReadWrite: {}});
+            const txId = txMeta.id as string;
+            await session.executeQuery(preparedQuery, {}, {txId});
+            await session.commitTransaction({txId});
+        });
+    } finally {
+        await driver.destroy();
+    }
+})();
