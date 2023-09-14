d3cbcc77f6c53ebe5baf7ab8aed166c840fd0cea
Alexey Zorkaltsev <AZorkaltsev@Yandex-Team.ru>
2023-09-14 18:59:41 +0300

    docs: add examples for the article

---
+++ examples/transaction-control/stale-read-only.ts
@@ -0,0 +1,16 @@
+import {Driver, TokenAuthService} from 'ydb-sdk';
+
+(async function () {
+    const driver = new Driver({
+        connectionString: process.env.YDB_CONNECTION_STRING,
+        authService: new TokenAuthService(process.env.YDB_TOKEN as string),
+    });
+    try {
+        await driver.tableClient.withSession(async (session) => {
+            const preparedQuery = await session.prepareQuery("SELECT 1");
+            await session.executeQuery(preparedQuery, {}, {beginTx: {staleReadOnly: {}}, commitTx: true});
+        });
+    } finally {
+        await driver.destroy();
+    }
+})();
