84ca64e992f7629ea55ab15446e7ee64dc274996
Alexey Zorkaltsev <azorkaltsev@yandex-team.com>
2023-12-13 06:27:12 +0300

    chore: eslint

--- eslint-local-rules/check/index.js
+++ eslint-local-rules/check/index.js
@@ -23,8 +23,12 @@ const eslint = new ESLint({
     // eslint-disable-next-line unicorn/prefer-module
     const expectedFilename = path.resolve(__dirname, 'tmp/expected.ts');
 
+    let cnt = 0;
+
     // eslint-disable-next-line no-unreachable-loop
     for (const v of variantsGenerator) {
+
+        cnt++;
 
         const fromSource = `/* eslint local-rules/context: "error", unicorn/no-static-only-class: "off" */\n${v.from.code}`;
         const expectedSource = `/* eslint unicorn/no-static-only-class: "off" */\n${v.to.code}`;
@@ -36,7 +40,8 @@ const eslint = new ESLint({
             writeFile(fromFilename, fromSource),
             // eslint-disable-next-line unicorn/prefer-module
             unlink(toFilename)
-                .catch(() => {}), // ignore 'no such file'
+                .catch(() => {
+                }), // ignore 'no such file'
             // eslint-disable-next-line unicorn/prefer-module
             writeFile(expectedFilename, expectedSource),
         ]);
@@ -60,4 +65,6 @@ const eslint = new ESLint({
 
         // break;
     }
+
+    console.info('cnt:', cnt);
 })();
