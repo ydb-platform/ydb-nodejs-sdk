aa5799df68ea982d195469f39a1baf6c31d575b3
Alexey Zorkaltsev <azorkaltsev@yandex-team.com>
2024-08-05 17:06:41 +0300

    chore: fix query service example

--- src/__tests__/e2e/table-service/graceful-session-close.test.ts
+++ src/__tests__/e2e/table-service/graceful-session-close.test.ts
@@ -6,7 +6,7 @@ import {initDriver, destroyDriver} from "../../../utils/test";
 
 const SHUTDOWN_URL = process.env.YDB_SHUTDOWN_URL || 'http://localhost:8765/actors/kqp_proxy?force_shutdown=all';
 
-xdescribe('Graceful session close', () => {
+describe('Graceful session close', () => {
 
     // TODO: Fix and enable test nce issue  will be resolved https://github.com/ydb-platform/ydb/issues/2981
     // TODO: Make the same test for query service
