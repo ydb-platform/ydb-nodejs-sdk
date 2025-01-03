ba7b32150a46acbaed4cc6d7b20425286190bf0b
Vladislav Polyakov <polRk@ydb.tech>
2025-01-03 11:09:41 +0000

    chore: add devcontainers setup with ydb
    Signed-off-by: Vladislav Polyakov <polRk@ydb.tech>

---
+++ .devcontainer/configure.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source /usr/local/share/nvm/nvm.sh && nvm use --lts
+
+if which ydb > /dev/null 2>&1; then
+    ENDPOINT=$(echo ${YDB_CONNECTION_STRING_SECURE:-$YDB_CONNECTION_STRING} | awk -F/ '{print $3}')
+    DATABASE=$(echo ${YDB_CONNECTION_STRING_SECURE:-$YDB_CONNECTION_STRING} | awk -F/ '{print "/" $4}')
+    CA_FILE_OPTION=""
+
+    if [ -n "$YDB_CONNECTION_STRING_SECURE" ]; then
+        CA_FILE_OPTION="--ca-file ${YDB_SSL_ROOT_CERTIFICATES_FILE}"
+    fi
+
+    ydb config profile create local \
+        --endpoint "$ENDPOINT" \
+        --database "$DATABASE" \
+        $CA_FILE_OPTION
+
+    ydb config profile activate local
+fi
