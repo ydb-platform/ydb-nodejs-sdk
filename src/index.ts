bde973e1041a9f676a7fff4815e90fa237b30482
Vladislav Polyakov <polRk@ydb.tech>
2025-01-07 07:13:05 +0000

    chore: migrate to modern module

--- src/index.ts
+++ src/index.ts
@@ -1,92 +1,12 @@
-export {Ydb} from 'ydb-sdk-proto';
+import type { TlsOptions } from "node:tls";
+import { Driver } from './driver.js'
 
-export {
-    getLogger,
-    setupLogger,
-    setDefaultLogger,
-    FallbackLogger,
-    getFallbackLogFunction,
-} from './logger/deprecated';
+export interface YDBConnectionOptions {
+    ssl?: TlsOptions
+    token?: string | ((driver: Driver) => Promise<string>),
+    balancer?: 'round_robin'
+}
 
-export {
-    Logger,
-    LogFn,
-} from './logger/simple-logger';
+export default function ydb(connectionString: string, { } = {}) {
 
-export {default as Driver, IDriverSettings, IPoolSettings} from './driver';
-
-export {
-    declareType,
-    StructFields,
-    Types,
-    TypedValues,
-    TypedData,
-    TypedDataOptions,
-    withTypeOptions,
-    NamesConversion,
-    snakeToCamelCaseConversion,
-    identityConversion,
-    primitiveTypeToValue,
-    typeMetadataKey,
-    getNameConverter,
-    StringFunction,
-} from './types';
-
-export {getCredentialsFromEnv, getSACredentialsFromJson} from './utils/parse-env-vars';
-export {ISslCredentials} from './utils/ssl-credentials';
-
-export {withRetries, RetryParameters} from './retries_obsoleted';
-export {Context} from './context';
-
-export {YdbError, StatusCode} from './errors';
-
-export {TableSessionPool} from "./table/table-session-pool";
-
-export {AlterTableDescription} from "./table/table-session";
-export {TableDescription} from "./table/table-session";
-export {TableIndex} from "./table/table-session";
-export {TableProfile} from "./table/table-session";
-export {CachingPolicy} from "./table/table-session";
-export {ExecutionPolicy} from "./table/table-session";
-export {CompactionPolicy} from "./table/table-session";
-export {ReplicationPolicy} from "./table/table-session";
-export {PartitioningPolicy} from "./table/table-session";
-export {ExplicitPartitions} from "./table/table-session";
-export {StoragePolicy} from "./table/table-session";
-export {ColumnFamilyPolicy} from "./table/table-session";
-export {StorageSettings} from "./table/table-session";
-export {Column} from "./table/table-session";
-export {TableSession, TableSession as Session} from "./table/table-session";
-export {ExecuteScanQuerySettings} from "./table/table-session";
-export {ReadTableSettings} from "./table/table-session";
-export {BulkUpsertSettings} from "./table/table-session";
-export {ExecuteQuerySettings} from "./table/table-session";
-export {PrepareQuerySettings} from "./table/table-session";
-export {RollbackTransactionSettings} from "./table/table-session";
-export {CommitTransactionSettings} from "./table/table-session";
-export {BeginTransactionSettings} from "./table/table-session";
-export {DescribeTableSettings} from "./table/table-session";
-export {DropTableSettings} from "./table/table-session";
-export {AlterTableSettings} from "./table/table-session";
-export {CreateTableSettings} from "./table/table-session";
-export {OperationParams} from "./table/table-session";
-export {AUTO_TX} from "./table/table-session";
-
-export {StaticCredentialsAuthService} from "./credentials/static-credentials-auth-service";
-export {IamAuthService} from "./credentials/iam-auth-service";
-export {MetadataAuthService} from "./credentials/metadata-auth-service";
-export {TokenAuthService} from "./credentials/token-auth-service";
-export {AnonymousAuthService} from "./credentials/anonymous-auth-service";
-export {ITokenService} from "./credentials/i-token-service";
-export {IAuthService} from "./credentials/i-auth-service";
-export {ModifyPermissionsSettings} from "./schema/scheme-service";
-export {DescribePathSettings} from "./schema/scheme-service";
-export {ListDirectorySettings} from "./schema/scheme-service";
-export {RemoveDirectorySettings} from "./schema/scheme-service";
-export {MakeDirectorySettings} from "./schema/scheme-service";
-
-export {ParsedConnectionString, parseConnectionString} from "./utils/parse-connection-string";
-export {QueryClient, ResultSet, RowType} from "./query";
-
-export {SimpleLogger} from "./logger/simple-logger";
-export {getDefaultLogger} from "./logger/get-default-logger";
+}
