70411f0cbef087df4c7eee51667e690b7db4b8eb
Vladislav Polyakov <polrk@ydb.tech>
2025-01-13 08:53:56 +0000

    chore: add ydb cluster devcontainers setup

--- .devcontainer/configure.sh
+++ .devcontainer/configure.sh
@@ -3,17 +3,18 @@
 source /usr/local/share/nvm/nvm.sh && nvm use --lts
 
 if which ydb > /dev/null 2>&1; then
-    ENDPOINT=$(echo ${YDB_CONNECTION_STRING_SECURE:-$YDB_CONNECTION_STRING} | awk -F/ '{print $3}')
-    DATABASE=$(echo ${YDB_CONNECTION_STRING_SECURE:-$YDB_CONNECTION_STRING} | awk -F/ '{print "/" $4}')
+    ENDPOINT=$(echo $YDB_CONNECTION_STRING | awk -F/ '{print $1 "//" $3}')
+    DATABASE=$(echo $YDB_CONNECTION_STRING | cut -d/ -f4-)
     CA_FILE_OPTION=""
 
-    if [ -n "$YDB_CONNECTION_STRING_SECURE" ]; then
+    if [ -n "$YDB_SSL_ROOT_CERTIFICATES_FILE" ]; then
+        ENDPOINT="${ENDPOINT/grpc:/grpcs:}"
         CA_FILE_OPTION="--ca-file ${YDB_SSL_ROOT_CERTIFICATES_FILE}"
     fi
 
-    ydb config profile create local \
+    ydb config profile replace local \
         --endpoint "$ENDPOINT" \
-        --database "$DATABASE" \
+        --database "/$DATABASE" \
         $CA_FILE_OPTION
 
     ydb config profile activate local
